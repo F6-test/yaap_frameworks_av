ArrowOS/android_frameworks_av

Camera: Allow setting fallback physical ID for logical camera [1/2]
On Redmi Note 8 (ginkgo):

 * miui camera uses logical id 61 as depth sensor on portrait mode
   but oss libcam maps it to physical id 2 which is wrong, our physical
   id of depth sensor is 20 so we must hack it this way

Set appropriate system property, for example:
persist.sys.camera.fallback_id_2=20

Change-Id: I58b88adc86dd0036fcbed34a7f20093f90e405dc

services/camera/libcameraservice/device3/hidl/HidlCamera3Device.cpp
@@ -29,6 +29,9 @@
 #define CLOGE(fmt, ...) ALOGE("Camera %s: %s: " fmt, mId.string(), __FUNCTION__, \
             ##__VA_ARGS__)
 
+#define CLOGW(fmt, ...) ALOGW("Camera %s: %s: " fmt, mId.string(), __FUNCTION__, \
+ ##__VA_ARGS__)
+
 // Convenience macros for transitioning to the error state
 #define SET_ERR(fmt, ...) setErrorState(   \
     "%s: " fmt, __FUNCTION__,              \
@@ -46,6 +49,7 @@
 #include <utils/Trace.h>
 #include <utils/Timers.h>
 #include <cutils/properties.h>
+#include <android-base/properties.h>
 
 #include <android/hardware/camera/device/3.7/ICameraInjectionSession.h>
 #include <android/hardware/camera2/ICameraDeviceUser.h>
@@ -180,10 +184,22 @@ status_t HidlCamera3Device::initialize(sp<CameraProviderManager> manager,
                     physicalId, /*overrideForPerfClass*/false, &mPhysicalDeviceInfoMap[physicalId],
                     /*overrideToPortrait*/false);
             if (res != OK) {
-                SET_ERR_L("Could not retrieve camera %s characteristics: %s (%d)",
-                        physicalId.c_str(), strerror(-res), res);
-                session->close();
-                return res;
+                auto fallbackId = base::GetProperty(
+                        "persist.sys.camera.fallback_id_" + physicalId, "");
+                if (!fallbackId.empty()) {
+                    CLOGW("Could not retrieve camera %s characteristics: %s (%d)",
+                            physicalId.c_str(), strerror(-res), res);
+                    CLOGW("Trying fallback camera %s", fallbackId.c_str());
+                    res = manager->getCameraCharacteristics(
+                            fallbackId, /*overrideForPerfClass*/false,
+                            &mPhysicalDeviceInfoMap[fallbackId], /*overrideToPortrait*/true);
+                }
+                if (res != OK) {
+                    SET_ERR_L("Could not retrieve camera %s characteristics: %s (%d)",
+                            physicalId.c_str(), strerror(-res), res);
+                    session->close();
+                    return res;
+                }
             }
 
             bool usePrecorrectArray =

F6-test/yaap_frameworks_av

Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

services/camera/libcameraservice/device3/hidl/HidlCamera3Device.cpp
@@ -29,6 +29,9 @@
 #define CLOGE(fmt, ...) ALOGE("Camera %s: %s: " fmt, mId.c_str(), __FUNCTION__, \
             ##__VA_ARGS__)
 
+#define CLOGW(fmt, ...) ALOGW("Camera %s: %s: " fmt, mId.c_str(), __FUNCTION__, \
+ ##__VA_ARGS__)
+
 // Convenience macros for transitioning to the error state
 #define SET_ERR(fmt, ...) setErrorState(   \
     "%s: " fmt, __FUNCTION__,              \
@@ -49,6 +52,7 @@
 #include <android/content/res/CameraCompatibilityInfo.h>
 #include <camera/StringUtils.h>
 #include <com_android_internal_camera_flags.h>
+#include <android-base/properties.h>
 
 #include <android/hardware/camera/device/3.7/ICameraInjectionSession.h>
 #include <android/hardware/camera2/ICameraDeviceUser.h>
@@ -185,10 +189,22 @@ status_t HidlCamera3Device::initialize(sp<CameraProviderManager> manager,
                     physicalId, /*overrideForPerfClass*/false, &mPhysicalDeviceInfoMap[physicalId],
                     CameraCompatibilityInfo());
             if (res != OK) {
-                SET_ERR_L("Could not retrieve camera %s characteristics: %s (%d)",
-                        physicalId.c_str(), strerror(-res), res);
-                session->close();
-                return res;
+                auto fallbackId = base::GetProperty(
+                        "persist.sys.camera.fallback_id_" + physicalId, "");
+                if (!fallbackId.empty()) {
+                    CLOGW("Could not retrieve camera %s characteristics: %s (%d)",
+                            physicalId.c_str(), strerror(-res), res);
+                    CLOGW("Trying fallback camera %s", fallbackId.c_str());
+                    res = manager->getCameraCharacteristics(
+                            fallbackId, /*overrideForPerfClass*/false,
+                            &mPhysicalDeviceInfoMap[fallbackId], /*overrideToPortrait*/true);
+                }
+                if (res != OK) {
+                    SET_ERR_L("Could not retrieve camera %s characteristics: %s (%d)",
+                            physicalId.c_str(), strerror(-res), res);
+                    session->close();
+                    return res;
+                }
             }
 
             bool usePrecorrectArray =